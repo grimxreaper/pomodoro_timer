fa2e6759a4b5e7f526f9afa2d53bc48bfafd01b8
Zahra <hizahrazadeh@gmail.com>
2021-03-21 16:38:20 -0400

    placed all limits

--- src/App.js
+++ src/App.js
@@ -1,130 +1,121 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import ReactDOM from "react-dom";
 
-
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.state = {
       break_length: 5,
-      session_length: 59,
-      time_left: "25:00"
-      
-    }
+      session_length: 25,
+      time_left: "25:00",
+    };
   }
-
 
   onClick = (button) => {
     const id = button.target.id;
-    console.log(id)
+    console.log(id);
     if (id === "start_stop") {
-      console.log("FIRST this WORKS!")
-      this.start_stop()
+      console.log("FIRST this WORKS!");
+      this.start_stop();
+    } else if (id === "break_decrement") {
+      console.log("FIRST BREAKS HERE");
+      this.break_decrement();
+    } else if (id === "break_increment") {
+      this.break_increment();
+    } else if (id === "session_decrement") {
+      this.session_decrement();
+    } else if (id === "session_increment") {
+      this.session_increment();
     }
-    else if (id === "break_decrement") {
-      console.log("FIRST BREAKS HERE")
-      this.break_decrement()
-    }
-    else if (id === "break_increment") {
-      this.break_increment()
-    }
-    else if (id === "session_decrement"){
-      this.session_decrement()
-    }
-    else if (id === "session_increment"){
-      this.session_increment()
-    }
-
-  }
+  };
 
   break_decrement = () => {
     // console.log("THEN WE BREAK HERE")
     // console.log(this.state.break_length)
     if (this.state.break_length > 0) {
       this.setState({
-        break_length: this.state.break_length - 1
-      })
+        break_length: this.state.break_length - 1,
+      });
     }
-  }
+  };
 
   break_increment = () => {
     if (this.state.break_length < 60) {
-    this.setState({
-      break_length: this.state.break_length + 1
-    })
-  }
-  }
+      this.setState({
+        break_length: this.state.break_length + 1,
+      });
+    }
+  };
 
   session_decrement = () => {
-    this.setState({
-      session_length: this.state.session_length - 1
-    })
-  }
+    if (this.state.session_length > 0)
+      this.setState({
+        session_length: this.state.session_length - 1,
+      });
+  };
 
   session_increment = () => {
     if (this.state.session_length < 60) {
-    this.setState({
-      session_length: this.state.session_length + 1
-    })
-  }
-    else if (this.state.break_length === 60) {
       this.setState({
-        session_length: 60
-      })
+        session_length: this.state.session_length + 1,
+      });
     }
-  
-  }
-
+  };
 
   start_stop = () => {
-    console.log("then this WORKS!")
+    console.log("then this WORKS!");
     this.setState({
       // fix this
-      time_left: this.state.session_length + ":00"
-    })
-  }
+      time_left: this.state.session_length + ":00",
+    });
+  };
   reset = () => {
     this.setState({
       break_length: 5,
-      session_length: 25
-    })
-  }
+      session_length: 25,
+    });
+  };
 
-  render(){
-    const { break_length, session_length, time_left} = this.props;
+  render() {
+    const { break_length, session_length, time_left } = this.props;
     return (
       <div>
         <div id="timer-label">
-        <h1>Pomodoro Timer</h1>
+          <h1>Pomodoro Timer</h1>
         </div>
 
         {/* decrement */}
-        <button id="break_decrement" onClick={this.onClick}>Decrease Break Length</button>
-        <button id="session_decrement" onClick={this.onClick}>Decrease Session Length</button>
+        <button id="break_decrement" onClick={this.onClick}>
+          Decrease Break Length
+        </button>
+        <button id="session_decrement" onClick={this.onClick}>
+          Decrease Session Length
+        </button>
         {/* increment */}
-        <button id="break_increment" onClick={this.onClick}>Increase Break Length</button>
-        <button id="session_increment" onClick={this.onClick}>Increase Session Length</button>
+        <button id="break_increment" onClick={this.onClick}>
+          Increase Break Length
+        </button>
+        <button id="session_increment" onClick={this.onClick}>
+          Increase Session Length
+        </button>
 
         <div id="break_length">{this.state.break_length}</div>
         <div id="session_length">{this.state.session_length}</div>
         <div id="time_left">{this.state.time_left}</div>
 
-        <button id="start_stop" onClick={this.onClick}> Start/Stop</button>
-        <button id="reset" onClick={this.onClick}> Reset </button>
-
-
+        <button id="start_stop" onClick={this.onClick}>
+          {" "}
+          Start/Stop
+        </button>
+        <button id="reset" onClick={this.onClick}>
+          {" "}
+          Reset{" "}
+        </button>
       </div>
-    )
+    );
   }
 }
 
-
-
-
-
-ReactDOM.render(<App />, document.getElementById('app'));
-
-
-
+ReactDOM.render(<App />, document.getElementById("app"));
