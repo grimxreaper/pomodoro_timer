a51d013e671b4398f4681403ad275075c70b6f10
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-23 23:09:02 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import ReactDOM from "react-dom";
 import { getByDisplayValue } from "@testing-library/react";
