a1c2fe18b2d5adee65f618129a5e7e147622b469
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-24 14:55:18 -0400

    debugging incrementer and decrementer

--- src/App.js
+++ src/App.js
@@ -370,27 +370,27 @@ class App extends React.Component {
   }
 }
 
-const SetTimer = (props) => {
-  const id = props.title.toLowerCase();
+// const SetTimer = (props) => {
+//   const id = props.title.toLowerCase();
   
-  return (
-    <div className="timer-container">
-      <h2 id={`${id}-label`}>
-        {props.title} Length
-      </h2>
-      <div className="flex actions-wrapper">
-        <button id={`${id}-decrement`} onClick={props.handleDecrease}>
-          <i className="fas fa-minus" />
-        </button>
+//   return (
+//     <div className="timer-container">
+//       <h2 id={`${id}-label`}>
+//         {props.title} Length
+//       </h2>
+//       <div className="flex actions-wrapper">
+//         <button id={`${id}-decrement`} onClick={props.handleDecrease}>
+//           <i className="fas fa-minus" />
+//         </button>
         
-        <span id={`${id}-length`}>{props.count}</span>
+//         <span id={`${id}-length`}>{props.count}</span>
         
-        <button id={`${id}-increment`} onClick={props.handleIncrease}>
-          <i className="fas fa-plus" />
-        </button>
-      </div>
-    </div>
-  );
-}
+//         <button id={`${id}-increment`} onClick={props.handleIncrease}>
+//           <i className="fas fa-plus" />
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
 ReactDOM.render(<App />, document.getElementById("app"));
 export default App;
