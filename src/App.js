c61aba439a4e6293d24a028f662715bbcbc97bd4
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-24 15:43:09 -0400

    commented out all code that is to be deleted

--- src/App.js
+++ src/App.js
@@ -59,68 +59,62 @@ class App extends React.Component {
     }
   };
 
-  break_decrement = () => {
-    if (this.state.break_length > 0) {
-      this.setState({
-        break_length: this.state.break_length - 1,
-      });
-    }
-  };
+  // break_decrement = () => {
+  //   if (this.state.break_length > 0) {
+  //     this.setState({
+  //       break_length: this.state.break_length - 1,
+  //     });
+  //   }
+  // };
 
-  break_increment = () => {
-    if (this.state.break_length < 60) {
-      this.setState({
-        break_length: this.state.break_length + 1,
-      });
-    }
-  };
+  // break_increment = () => {
+  //   if (this.state.break_length < 60) {
+  //     this.setState({
+  //       break_length: this.state.break_length + 1,
+  //     });
+  //   }
+  // };
 
-  session_decrement = () => {
-    if (this.state.session_length > 0)
-      this.setState({
-        session_length: this.state.session_length - 1,
-        minutes: this.state.session_length - 1,
-        clockCount: this.state.clockCount - 1,
-      });
-  };
+  // session_decrement = () => {
+  //   if (this.state.session_length > 0)
+  //     this.setState({
+  //       session_length: this.state.session_length - 1,
+  //       minutes: this.state.session_length - 1,
+  //     });
+  // };
 
-  session_increment = () => {
-    if (this.state.session_length < 60) {
-      this.setState({
-        session_length: this.state.session_length + 1,
-        minutes: this.state.session_length + 1,
-      });
-    }
-  };
+  // session_increment = () => {
+  //   if (this.state.session_length < 60) {
+  //     this.setState({
+  //       session_length: this.state.session_length + 1,
+  //       minutes: this.state.session_length + 1,
+  //     });
+  //   }
+  // };
 
   handleLengthChange = (count, timerType) => {
-    const { 
-      session_length, 
-      break_length, 
-      isPlaying, 
-      cycle
-    } = this.state;
-    
+    const { session_length, break_length, isPlaying, cycle } = this.state;
+
     let newCount;
-    
-    if(timerType === 'session') {
+
+    if (timerType === "session") {
       newCount = session_length + count;
     } else {
       newCount = break_length + count;
     }
-    
-    if(newCount > 0 && newCount < 61 && !isPlaying) {
+
+    if (newCount > 0 && newCount < 61 && !isPlaying) {
       this.setState({
-        [`${timerType}_length`]: newCount
+        [`${timerType}_length`]: newCount,
       });
-      
-      if(cycle.toLowerCase() === timerType) {
+
+      if (cycle.toLowerCase() === timerType) {
         this.setState({
-          clockCount: newCount * 60
-        })
+          clockCount: newCount * 60,
+        });
       }
     }
-  }
+  };
 
   start_stop = () => {
     const { countdown } = this.state;
@@ -135,9 +129,6 @@ class App extends React.Component {
         const {
           break_length,
           session_length,
-          minutes,
-          play,
-          seconds,
           cycle,
           countdown,
           clockCount,
@@ -254,48 +245,37 @@ class App extends React.Component {
   };
 
   render() {
-    const { 
-      break_length,
-      session_length,
-      minutes,
-      play,
-      seconds,
-      cycle,
-      countdown,
-      clockCount,
-      handleDecrease,
-      handleIncrease
-    } = this.state;
-    
+    const { break_length, session_length, cycle, clockCount } = this.state;
+
     const breakProps = {
-      title: 'Break',
+      title: "Break",
       count: break_length,
-      handleDecrease: () => this.handleLengthChange(-1, 'break'),
-      handleIncrease: () => this.handleLengthChange(1, 'break')
-    }
-    
+      handleDecrease: () => this.handleLengthChange(-1, "break"),
+      handleIncrease: () => this.handleLengthChange(1, "break"),
+    };
+
     const sessionProps = {
-      title: 'Session',
+      title: "Session",
       count: session_length,
-      handleDecrease: () => this.handleLengthChange(-1, 'session'),
-      handleIncrease: () => this.handleLengthChange(1, 'session'),
-    }
+      handleDecrease: () => this.handleLengthChange(-1, "session"),
+      handleIncrease: () => this.handleLengthChange(1, "session"),
+    };
 
     return (
       <div>
-          <h1 id="page-title">Pomodoro Timer</h1>
-          <div id="timer-label">
-             <h2 id="mainLabel"> you got this! </h2></div>
-        <div className="flex">
-          <SetTimer {...breakProps} />
-          <SetTimer {...sessionProps} />
+        <h1 id="page-title">Pomodoro Timer</h1>
+        <div id="timer-label">
+          <h2 id="mainLabel"> you got this! </h2>
         </div>
-        
+          <div className="flex">
+            <SetTimer {...breakProps} />
+            <SetTimer {...sessionProps} />
+          </div>
+
         <div id="time-left">
           <h1 id="timer-label">{cycle}</h1>
           <span className="timer">{this.convertToTime(clockCount)}</span>
-          
-          
+
           <div className="flex">
             <button id="start_stop" onClick={this.start_stop}>
               play/pause
@@ -303,7 +283,7 @@ class App extends React.Component {
             <button id="reset" onClick={this.reset}>
               reset
             </button>
-             <audio
+            <audio
               src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
               id="beep"
               ref={this.audio}
@@ -311,7 +291,8 @@ class App extends React.Component {
             />
           </div>
         </div>
-      </div>);
+      </div>
+    );
 
     // return (
     //   <div>
@@ -404,23 +385,21 @@ class App extends React.Component {
 
 const SetTimer = (props) => {
   const id = props.title.toLowerCase();
-  
+
   return (
     <div className="timer-container">
-      <h2 id={`${id}-label`}>
-        {props.title} Length
-      </h2>
+      <h2 id={`${id}-label`}>{props.title} Length</h2>
       <div className="flex actions-wrapper">
         <button id={`${id}-decrement`} onClick={props.handleDecrease}>
           <FontAwesomeIcon
-          icon={faSortDown}
-          id="iconSdown"
-          onClick={props.handleDecrease}
-         />
+            icon={faSortDown}
+            id="iconSdown"
+            onClick={props.handleDecrease}
+          />
         </button>
-        
+
         <span id={`${id}-length`}>{props.count}</span>
-        
+
         <button id={`${id}-increment`} onClick={props.handleIncrease}>
           <FontAwesomeIcon
             icon={faSortUp}
@@ -431,6 +410,6 @@ const SetTimer = (props) => {
       </div>
     </div>
   );
-}
+};
 ReactDOM.render(<App />, document.getElementById("app"));
 export default App;
