3b91657a319c33d9896c05793c2d2a5f5cfbb5e4
Zahra Dabirzadeh <hizahrazadeh@gmail.com>
2021-03-23 23:10:23 -0400

    new try for audio

--- src/App.js
+++ src/App.js
@@ -19,6 +19,43 @@ class App extends React.Component {
       countdown: false,
     };
   }
+
+
+  useAudio = url => {
+    const [audio] = useState(new Audio(url));
+    const [playing, setPlaying] = useState(false);
+  
+    const toggle = () => setPlaying(!playing);
+  
+    useEffect(() => {
+        playing ? audio.play() : audio.pause();
+      },
+      [playing]
+    );
+  
+    useEffect(() => {
+      audio.addEventListener('ended', () => setPlaying(false));
+      return () => {
+        audio.removeEventListener('ended', () => setPlaying(false));
+      };
+    }, []);
+  
+    return [playing, toggle];
+  };
+  
+  Player = ({ url }) => {
+    const [playing, toggle] = useAudio(url);
+
+    return (
+      <div>
+        <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+      </div>
+    );
+  };
+
+
+
+
   audio = new Audio(
     "https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
   );
